8b8e3b6efcdfe35f8428c4efdf5fb38d02da90e3
byzz <eyedroot@gmail.com>
2023-05-07 14:23:18 +0900

    update: FontAwesome 라이브러리 추가 및 파일 분리

---
+++ src/fontawsome-icon.js
@@ -0,0 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faClock)
+
+export default FontAwesomeIcon
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import dayjs from "dayjs"
+import 'dayjs/locale/ko'
+import 'dayjs/locale/en'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+import FontAwesomeIcon from '@/fontawsome-icon'
+
 import './assets/style/tailwind.css'
 
-createApp(App).mount('#app')
+dayjs.extend(relativeTime)
+dayjs.locale('en')
+
+const app = createApp(App);
+
+app.config.globalProperties.$dayjs = dayjs
+
+app.component('FaIcon', FontAwesomeIcon)
+
+app.mount('#app')
