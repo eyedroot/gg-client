760da508dedc6d8481b884b0ab2cf714fe961f2d
byzz <eyedroot@gmail.com>
2023-05-10 00:10:31 +0900

    update: timeAgo

--- src/components/dump/rows/TimeAgo.vue
+++ src/components/dump/rows/TimeAgo.vue
@@ -1,23 +1,40 @@
 <template>
   <span class="text-[11px] tracking-tight text-gray-700">
-    {{ getFromNow }}
+    {{ timeAgo }}
   </span>
 </template>
 
 <script>
 export default {
   name: 'TimeAgo',
-  props: {
-    now: {
-      type: Date,
-      default: () => new Date(),
-      required: false
+  data() {
+    return {
+      timerId: null,
+      time: new Date(),
     }
   },
+  mounted() {
+    this.timerId = setInterval(() => {
+      this.time = new Date()
+    }, 1000)
+  },
   computed: {
-    getFromNow() {
-      return this.$dayjs(this.now).fromNow()
+    timeAgo() {
+      const diff = Math.floor((new Date() - this.time) / 1000)
+
+      if (diff < 60) {
+        return `${diff} seconds ago`
+      } else if (diff < 3600) {
+        return `${Math.floor(diff / 60)} minutes ago`
+      } else if (diff < 86400) {
+        return `${Math.floor(diff / 3600)} hours ago`
+      } else {
+        return `${Math.floor(diff / 86400)} days ago`
+      }
     }
+  },
+  beforeUnmount() {
+    clearInterval(this.timerId)
   }
 }
 </script>
