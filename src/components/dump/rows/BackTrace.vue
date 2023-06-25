804ae00c55d507bc78150628ef264e893f3118be
byzz <eyedroot@gmail.com>
2023-06-25 18:09:27 +0900

    refactor

--- src/components/dump/rows/BackTrace.vue
+++ src/components/dump/rows/BackTrace.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="--backtrace flex flex-col px-1.5 pt-1 tracking-tighter overflow-x-auto">
-    <div :key="index"
-      v-for="(row, index) in backtrace"
-      class="flex flex-col">
-        <div class="flex flex-row items-center text-gray-600 hover:text-gray-900 hover:bg-gray-100 pl-2 leading-6">
-          <span class="inline-flex items-center justify-center w-4 h-4 bg-emerald-600 hover:bg-emerald-700 transition rounded mr-1.5 text-white cursor-pointer">+</span>
+    <div :key="index" v-for="(row, index) in backtrace" class="flex flex-col">
+      <div class="flex flex-row items-center text-gray-600 hover:text-gray-900 hover:bg-gray-100 pl-2 leading-6">
+        <span class="inline-flex items-center justify-center w-4 h-4 bg-emerald-600 hover:bg-emerald-700 transition rounded mr-1.5 text-white cursor-pointer">+</span>
 
-          <span :class="{ 'text-gray-700 font-bold': row.file === this.focusFile.file }">
-            {{ row?.file ? row.file : row?.class }}:<strong>{{ row?.line }}</strong>
-          </span>
+        <span :class="{ 'text-gray-700 font-bold': row.file === this.focusFile.file }">
+          {{ row?.file ? row.file : row?.class }}:<strong>{{ row?.line }}</strong>
+        </span>
 
-          <code class="ml-1.5 text-blue-600 font-bold">
-            {{ row?.function }}(<span
-            class="inline-flex bg-gray-300 rounded-sm px-0.5 cursor-pointer text-black select-none"
-            @click="toggleParameters(index)">...</span>)
-          </code>
-        </div>
+        <code class="ml-1.5 text-blue-600 font-bold">
+          {{ row?.function }}(<span
+          class="inline-flex bg-gray-300 rounded-sm px-0.5 cursor-pointer text-black select-none"
+          @click="toggleParameters(index)">...</span>)
+        </code>
+      </div>
 
-        <div class="--code ml-5 my-2" :class="{ 'hidden': ! this.showParametersIndexes.includes(index) }">
-          <component :is="this.$getValueComponent(row?.args)" :capsule-dto="row?.args"></component>
-        </div>
+      <div class="--code ml-5 my-2" :class="{ 'hidden': ! this.showParametersIndexes.includes(index) }">
+        <component :is="this.$getValueComponent(row?.args)" :capsule-dto="row?.args"></component>
+      </div>
     </div>
   </div>
 </template>
