e588cad3e20ff8f5f44e6ed66d3945273caf8e10
byzz <eyedroot@gmail.com>
2023-06-28 00:36:33 +0900

    Update BackTrace row parameter toggle styles
    Add 'select-none' class to SideNavigation and ScrollableOptions components to prevent text selection. Improve both components' UX by disabling text selection, making interaction more seamless.

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="flex flex-col w-[45px] bg-gray-200 border-r-[1px] border-gray-300 py-5 px-1.5 items-center justify-center">
+  <aside class="flex flex-col w-[45px] bg-gray-200 border-r-[1px] border-gray-300 py-5 px-1.5 items-center justify-center select-none">
     <div class="flex flex-col w-full space-y-3.5">
       <span class="relative inline-flex">
         <SideNavigationBalloon :notice-count="noticeCount.logContainer"></SideNavigationBalloon>
