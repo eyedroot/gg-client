e28298506ba11950b5d9721c364cd95616527478
byzz <eyedroot@gmail.com>
2023-07-20 14:25:17 +0900

    "Add timestamp feature and refactor RowExtensions component"
    This commit adds a visual timestamp feature on RowExtensions.vue component by leveraging vue's computed property for this purpose. Now, the current date-time is displayed next to a blue circle which gradually fades out over 30 seconds, providing a nice visual effect.
    
    On the refactoring end, an adjustment was made to determine whether data is local or not, changing from "isLocalData" to "props.isLocalData". Also, the component changed from options API to composition API using <script setup> and the separate 'saved' reactive property was created. This provides more clarity and efficiency in our code execution. Animation for the fade-out feature was added on SCSS block.

--- src/components/dump/RowExtensions.vue
+++ src/components/dump/RowExtensions.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="flex flex-row space-x-2.5 justify-start items-center">
+    <div class="flex items-center space-x-2">
+      <span class="rounded-full bg-blue-700 w-2 h-2 fade-out"></span>
+      <span class="text-gray-800">{{ currentDateTime }}</span>
+    </div>
+
     <span class="text-gray-500 text-[80%]">
       <slot name="languageVersion"></slot>
     </span>
@@ -10,7 +15,7 @@
       <span class="text-gray-500">png</span>
     </button>
 
-    <button v-if="isLocalData === false"
+    <button v-if="props.isLocalData === false"
             class="--backtrace-button button transparent inline-flex items-center justify-center"
             @click="this.$emit('saveToLocalStorage'); this.saved = true">
       <fa-icon icon="floppy-disk" class="mr-1"></fa-icon>
@@ -25,22 +30,45 @@
   </div>
 </template>
 
-<script>
-import {ref} from "vue"
+<script setup>
+import {computed, ref} from "vue"
 
-export default {
-  props: {
-    isLocalData: {
-      type: Boolean,
-      required: true,
-    },
+const props = defineProps({
+  isLocalData: {
+    type: Boolean,
+    required: true,
   },
-  setup() {
-    const saved = ref(false)
+})
 
-    return {
-      saved,
-    }
-  },
-}
+const saved = ref(false)
+
+const currentDateTime = computed(() => {
+  const date = new Date()
+
+  const year = date.getFullYear()
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const day = ('0' + (date.getDate())).slice(-2)
+
+  const hour = ('0' + (date.getHours())).slice(-2)
+  const minute = ('0' + (date.getMinutes())).slice(-2)
+  const second = ('0' + (date.getSeconds())).slice(-2)
+
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+})
 </script>
+
+<style lang="scss">
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+.fade-out {
+  animation: fadeOut 30s forwards;
+}
+</style>
