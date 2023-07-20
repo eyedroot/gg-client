308acd684983d3b3b3485550b8aa7a3fd6ad1b96
byzz <eyedroot@gmail.com>
2023-07-20 22:42:57 +0900

    "Implement dark mode styles in several components"
    This commit adds support for dark mode across various components by applying suitable color changes to text, backgrounds, borders, and other visual elements. This change ensures that the application will have a consistent appearance under both light and dark themes, enhancing the user experience.

--- src/components/dump/rows/CodeViewer.vue
+++ src/components/dump/rows/CodeViewer.vue
@@ -3,7 +3,10 @@
     <div class="flex flex-col">
       <div v-for="(row, idx) in sourceCode" :key="idx" class="flex flex-row">
         <div class="flex select-none justify-end min-w-[20px] text-gray-400 mr-3">{{ row.line }}</div>
-        <code class="flex-1 whitespace-pre text-gray-700" :class="{ 'bg-gray-200': row.line === focusLine }" v-html="highlightCode(row.code)"></code>
+        <code
+          class="flex-1 whitespace-pre text-gray-700 dark:text-gray-200"
+          :class="{ 'bg-gray-200 dark:bg-gray-900': row.line === focusLine }"
+          v-html="highlightCode(row.code)"></code>
       </div>
     </div>
   </div>
--- src/components/dump/values/ThrowableValue.vue
+++ src/components/dump/values/ThrowableValue.vue
@@ -2,15 +2,15 @@
   <div class="--code throwable">
     <div class="flex flex-row space-x-5">
       <span class="--title">Class</span>
-      <span class="--value">{{ messageDto.data.class }}</span>
+      <span class="--value dark:text-gray-200">{{ messageDto.data.class }}</span>
     </div>
     <div class="flex flex-row space-x-5">
       <span class="--title">File</span>
-      <span class="--value">{{ messageDto.data.value.file }}:{{ messageDto.data.value.line }}</span>
+      <span class="--value dark:text-gray-200">{{ messageDto.data.value.file }}:{{ messageDto.data.value.line }}</span>
     </div>
     <div class="flex flex-row space-x-5">
       <span class="--title">Message</span>
-      <span class="--value text-orange-600">{{ messageDto.data.value.message }} (CODE: {{ messageDto.data.value.code }})</span>
+      <span class="--value text-orange-600 font-bold dark:text-orange-300">{{ messageDto.data.value.message }} (CODE: {{ messageDto.data.value.code }})</span>
     </div>
 
     <CodeViewer :focus-line="codeViewerFile.line" :source-code="codeViewerFile.sourceCode"></CodeViewer>
@@ -47,8 +47,9 @@ export default {
 <style scoped lang="scss">
 .--title {
   @apply flex font-medium text-gray-500 uppercase tracking-wider w-20 justify-end py-1;
+  @apply dark:text-gray-300;
 }
 .--value {
-  @apply flex-1 font-medium py-1;
+  @apply flex-1 py-1;
 }
 </style>
