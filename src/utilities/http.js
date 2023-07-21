4c1b75d1f93f0f869b46cf7d4bfc5c0563807f73
byzz <eyedroot@gmail.com>
2023-07-21 17:46:44 +0900

    Update http.js file indentation
    The indentation of the JavaScript object in the http.js file has been updated. The original indentation was inconsistent causing readability issues. This commit doesn't change any functionality, just improves code structure for better clarity.

--- src/utilities/http.js
+++ src/utilities/http.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 const http = axios.create({
-    baseURL: "https://phpgg.kr/api",
-    headers: {
-        "Content-type": "application/json",
-    },
+  baseURL: "https://phpgg.kr/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 })
 
 export default http
