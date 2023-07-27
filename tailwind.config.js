6c039e6f8f799f6a6e9883cca331a97620620961
byzz <eyedroot@gmail.com>
2023-07-27 15:32:42 +0900

    update: tailwind color presets as bluegray

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bluegray-900': '#0F172A',
+        'bluegray-800': '#1E293B',
+        'bluegray-700': '#334155',
+        'bluegray-600': '#475569',
+        'bluegray-500': '#64748B',
+        'bluegray-400': '#94A3B8',
+        'bluegray-300': '#CBD5E1',
+        'bluegray-200': '#E2E8F0',
+        'bluegray-100': '#F1F5F9',
+        'bluegray-50': '#F8FAFC',
+      }
     },
   },
   plugins: [],
