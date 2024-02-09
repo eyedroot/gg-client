dc74b755e71af368d04205da1f314afcdc2f85dd
byzz <eyedroot@gmail.com>
2024-02-09 14:42:54 +0900

    Revert "update: LineNumber Comp"
    This reverts commit 111d7c8ab4be64d7a0d88c75fabfe303b15ae897.

--- src/components/fragments/LineNumber.vue
+++ src/components/fragments/LineNumber.vue
@@ -1,26 +1,12 @@
 <template>
-  <div class="text-green-700 dark:text-green-500 mb-1.5">
-    /** #{{ props.displayId }} - {{ currentDateTime }} */
-  </div>
+  <div class="text-green-700 dark:text-green-500 mb-1.5">/** #{{ props.displayId }} */</div>
 </template>
 
 <script setup>
-import {computed} from "vue";
-
 const props = defineProps({
   displayId: {
     type: Number,
-    required: true,
+    required: true
   },
-})
-
-const currentDateTime = computed(() => {
-  const date = new Date()
-
-  const hour = ('0' + (date.getHours())).slice(-2)
-  const minute = ('0' + (date.getMinutes())).slice(-2)
-  const second = ('0' + (date.getSeconds())).slice(-2)
-
-  return `${hour}:${minute}:${second}`
 })
 </script>
