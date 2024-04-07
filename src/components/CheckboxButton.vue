f366c62a0c1fc111b0022b6beec4bd490a541611
byzz <eyedroot@gmail.com>
2024-04-07 15:12:22 +0900

    update: 체크박스 활성화 상태 반응형 적용

--- src/components/CheckboxButton.vue
+++ src/components/CheckboxButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
 
 const props = defineProps({
   label: {
@@ -7,14 +7,19 @@ const props = defineProps({
     required: true,
   },
 });
+
+const isChecked = ref(false);
 </script>
 
 <template>
   <label
-    class="flex items-center justify-center rounded-full text-xs tracking-tighter bg-gray-300 cursor-pointer transition-colors duration-200 px-1.5 font-semibold text-gray-400 border-2 border-gray-500"
+    class="flex items-center justify-center rounded-full text-xs tracking-tighter bg-gray-300 cursor-pointer transition-colors duration-200 px-1.5 font-semibold text-gray-400 border-2 border-gray-400"
+    :class="{
+      'bg-gray-400 text-gray-800 border-gray-800': isChecked,
+    }"
   >
     {{ props.label }}
-    <input type="checkbox" class="hidden" />
+    <input type="checkbox" class="hidden" v-model="isChecked" />
   </label>
 </template>
 
